3f361bc8625e4eb82265e7b698f1123d41596917
happyTaco27 <36053877+happyTaco27@users.noreply.github.com>
2018-05-15 15:17:19 -0600

    added class stuff

--- Donovan's_stuff/template/sketch.js
+++ Donovan's_stuff/template/sketch.js
@@ -1,34 +1,40 @@
-// p5js template project - replace with project title
-// Dan Schellenberg - replace with your name
-// Feb 2, 2018 - replace with the date
-
-// global variables
-let gear;
-
-// the preload function guarentees that the code inside the function is
-// executed before the rest of the program runs -- helpful for things
-// like loading images (since JS is asynchronous)
-function preload() {
-  gear = loadImage("images/gear.png");
-}
-
-// the setup function will only run once (before the draw loop begins)
-// this is where you want to set up the environment (size of canvas, etc)
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  background(255);
-
-  image(gear, 0, 0);
-
-  stroke(0);
-  line(0, 0, 200, 200);
-
-  fill(0, 255, 0, 100);
-  noStroke();
-
-  rect(mouseX, mouseY, 100, 300);
-  ellipse(400, 150, 300, 200);
+class Player{
+  stats(){
+    let level = 1
+    let experience = 0;
+    "maxHp" : 100,
+    "currentHp" : 100,
+    "maxEnergy" : 100,
+    "currentEnergy" : 100,
+    "maxMana" : 100,
+    "currentMana" : 100,
+    "gold" : 100,
+    "diamonds" : 0,
+  },
+  skills : {
+    fighting : {
+      "lvl" : 1,
+      "exp" : 0,
+    },
+    spellcasting : {
+      "lvl" : 1,
+      "exp" : 0,
+    },
+    archery : {
+      "lvl" : 1,
+      "exp" : 0,
+    },
+    fishing : {
+      "lvl" : 1,
+      "exp" : 0,
+    },
+    mining : {
+      "lvl" : 1,
+      "exp" : 0,
+    },
+    crafting : {
+      "lvl" : 1,
+      "exp" : 0,
+    },
+  }
 }
