548b3b5a6133e4b71e4b7c5e08c64f1b14cc97bc
happyTaco27 <36053877+happyTaco27@users.noreply.github.com>
2018-05-16 15:18:49 -0600

    adding classes

--- Donovan's_stuff/template/sketch.js
+++ Donovan's_stuff/template/sketch.js
@@ -1,40 +1,66 @@
-class Player{
-  stats(){
-    let level = 1
-    let experience = 0;
-    "maxHp" : 100,
-    "currentHp" : 100,
-    "maxEnergy" : 100,
-    "currentEnergy" : 100,
-    "maxMana" : 100,
-    "currentMana" : 100,
-    "gold" : 100,
-    "diamonds" : 0,
-  },
-  skills : {
-    fighting : {
-      "lvl" : 1,
-      "exp" : 0,
-    },
-    spellcasting : {
-      "lvl" : 1,
-      "exp" : 0,
-    },
-    archery : {
-      "lvl" : 1,
-      "exp" : 0,
-    },
-    fishing : {
-      "lvl" : 1,
-      "exp" : 0,
-    },
-    mining : {
-      "lvl" : 1,
-      "exp" : 0,
-    },
-    crafting : {
-      "lvl" : 1,
-      "exp" : 0,
-    },
+//stat calculator
+let s,p,e,t,i,a,l;
+let s1,p1,e1,t1,i1,a1,l1;
+let bonus,playerN,aiN;
+let playerRole,aiRole;
+
+
+// let sCheck,pCheck,eCheck,tCheck,iCheck,aCheck,lCheck;
+class StatCheck {
+  constructor() {
+    this.playerRole;
+    this.aiRole;
+  }
+  d20(){
+    let roll=round(random(20));
+    print("Thing");
+    return roll;
+
+  }
+  skillCheck(n){
+    if(n<5){
+      bonus= -1;
+    }
+    else if(n<10){
+      bonus=1;
+    }
+    else if(n<15){
+      bonus=2;
+    }
+    else if(n<20){
+      bonus=3;
+    }
+    else if(n===20){
+      bonus=4;
+    }
+  }
+  aiSkillCheck(n){
+    if(n<5){
+      bonus= -1;
+    }
+    else if(n<10){
+      bonus=1;
+    }
+    else if(n<15){
+      bonus=2;
+    }
+    else if(n<20){
+      bonus=3;
+    }
+    else if(n===20){
+      bonus=4;
+    }
+  }
+  // checkingIfPlayersRoleIsbetter(){
+  //   if(playerRole+playerN<aiRole+aiN){
+  //
+  //   }
+  // }
+}
+class Health {
+  constructor() {
+    this.playerVal;
+    this.aiVal;
+
   }
 }
