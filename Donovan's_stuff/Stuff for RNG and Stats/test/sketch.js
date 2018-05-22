42f1204f6479e1fad308f35eda646c60dc8eab28
happyTaco27 <36053877+happyTaco27@users.noreply.github.com>
2018-05-22 08:32:08 -0600

    updating files files

--- Donovan's_stuff/Stuff for RNG and Stats/test/sketch.js
+++ Donovan's_stuff/Stuff for RNG and Stats/test/sketch.js
@@ -5,6 +5,8 @@ let softSkillsPoints;
 let exp,lvl,currentExp,expEarned,expToLevelUp;
 let s,p,e,t,i,a,l,newS,newP,newE,newT,newI,newA,newL;
 let sizeOfSkillTile;
+let ifInventoryIsOpen=false;
+let d20;
 sizeOfSkillTile=100;
 newS=s;
 newP=p;
@@ -16,6 +18,7 @@ newL=l;
 lvl=1;
 exp=0;
 softSkillsPoints=5;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   //base level exp requirements
@@ -29,43 +32,49 @@ function setup() {
   a=round(random(20));
   l=round(random(20));
   print("S:",s," P:",p," E:",e," T:",t," I:",i," A:",a," L:",l);
+  print("Soft Skill Point Remaining:",softSkillsPoints);
+  (document).on("keypress", function(event) {
+    if (event.keyCode === 9) {   //tab pressed
+      return false; // stops its action
+    }
+  });
 }
-
 function draw() {
   levelUp();
+  skillTree();
+  background(255);
 }
 function statCheck(){
-//This is for later when i have hit reg. to test and the stats work as they should
 }
 function levelUp(){
+  let lvlUpD20=round(random(20));
   //As the name implies, for Character level up and core testing of Stats
   if(exp===expToLevelUp){
     lvl++;
-    softSkillsPoints++;
     expToLevelUp=round(expToLevelUp*1.5);
     exp=0;
+    if(round(lvl%3)===0){
+      softSkillsPoints++;
+    }
     print("Exp needed: ",expToLevelUp," Level: ",lvl);
     print("S:",s," P:",p," E:",e," T:",t," I:",i," A:",a," L:",l);
+    print("Current Soft Skill Points:",softSkillsPoints);
   }
 }
-
-function stats()  {
 //haven't found a use for this yet
+function stats()  {
 }
 //This is for the soft skills and feats
 function skillTree()   {
-//This is just a place-holder
   let teir1,teir2,teir3,teir4,teir5,teir6;
   let pacifist=[];
   let neutral=[];
   let anarchist=[];
-
   fill(0);
   rect(0,0,width,height);
 }
-
+// key binds
 function keyPressed(){
-  //For Skill Tree
   if(keyCode===TAB){
     skillTree();
   }
